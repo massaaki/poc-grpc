19a194e6cc822ee5b83fa239e1a61447bc05569e
massaaki <eu@massaaki.com>
2022-01-12 08:51:34 -0300

    configure && start the server

--- Basic Examples/04 - Bidirecional Streaming Example/src/contracts/chat.ts
+++ Basic Examples/04 - Bidirecional Streaming Example/src/contracts/chat.ts
@@ -1,15 +1,15 @@
 import type * as grpc from '@grpc/grpc-js';
 import type { MessageTypeDefinition } from '@grpc/proto-loader';
 
-import type { ChatClient as _chatPackate_ChatClient, ChatDefinition as _chatPackate_ChatDefinition } from './chatPackate/Chat';
+import type { ChatClient as _chatPackage_ChatClient, ChatDefinition as _chatPackage_ChatDefinition } from './chatPackage/Chat';
 
 type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
   new(...args: ConstructorParameters<Constructor>): Subtype;
 };
 
 export interface ProtoGrpcType {
-  chatPackate: {
-    Chat: SubtypeConstructor<typeof grpc.Client, _chatPackate_ChatClient> & { service: _chatPackate_ChatDefinition }
+  chatPackage: {
+    Chat: SubtypeConstructor<typeof grpc.Client, _chatPackage_ChatClient> & { service: _chatPackage_ChatDefinition }
     ChatRequest: MessageTypeDefinition
     ChatResponse: MessageTypeDefinition
   }
--- Basic Examples/04 - Bidirecional Streaming Example/src/server.ts
+++ Basic Examples/04 - Bidirecional Streaming Example/src/server.ts
@@ -1,1 +1,73 @@
-console.log('server');
+import path from "path";
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+
+import { ChatHandlers } from "contracts/chatPackate/Chat";
+import { ProtoGrpcType as ChatType } from "contracts/chat";
+
+import { ChatRequest } from "contracts/chatPackate/ChatRequest";
+import { ChatResponse } from "contracts/chatPackate/ChatResponse";
+
+
+const PORT = 50051;
+const chatPackageDef = protoLoader.loadSync(path.resolve(__dirname, 'contracts', 'chat.proto'));
+const chatGrpcObj = (grpc.loadPackageDefinition(chatPackageDef) as unknown) as ChatType;
+const chatPackage = chatGrpcObj.chatPackage;
+
+
+// Global vars
+const callObjByUsername = new Map<string, grpc.ServerDuplexStream<ChatRequest, ChatResponse>>()
+
+
+function configServer() {
+  const server = new grpc.Server()
+
+  server.addService(
+    chatPackage.Chat.service,
+    {
+      Chat: (call) => {
+        call.on("data", (request) => {
+          const username = call.metadata.get('username')[0] as string;
+          const message = request.message;
+
+          for (let [user, usersCall] of callObjByUsername) {
+            if (username !== user) {
+              usersCall.write({
+                username,
+                message
+              })
+            }
+          }
+
+          if (callObjByUsername.get(username) === undefined) {
+            callObjByUsername.set(username, call);
+          }
+        });
+
+        call.on("end", () => {
+          const username = call.metadata.get('username')[0] as string;
+          callObjByUsername.delete(username)
+          call.write({
+            username: "Server",
+            message: `End message ${username}`
+          })
+
+          call.end();
+        });
+      }
+    } as ChatHandlers);
+
+  return server;
+}
+
+function main() {
+  const server = configServer();
+
+  server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (error, port) => {
+    if (error) {
+      console.log('Error to start the server ', error);
+    }
+    console.log("Server is running on port ", port);
+  })
+};
+main();
